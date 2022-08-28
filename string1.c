16bc54c35639bd3c64920f411fd76ef1648aa680
Progress ochuko Eyaadah <koxygraphicsgmail.com>
2022-08-28 10:55:28 -0700

    done

---
+++ string1.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		
+	}
+	dest[i++] = src[i];
+
+	return (dest);
+}
+
+/**
+ *
